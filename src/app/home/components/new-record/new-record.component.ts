d3c94dccff7d4651d1c5ae36c88ee8fd9e4d6967
mognia <mognia420@gmail.com>
2019-03-24 20:34:48 +0430

    bugFix - add first form control
    its not working yet

--- src/app/home/components/new-record/new-record.component.ts
+++ src/app/home/components/new-record/new-record.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input , EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FileInputService } from "../../../services/file-input.service";
 import { UploaderComponent } from "./../uploader/uploader.component";
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'new-record',
   templateUrl: './new-record.component.html',
@@ -8,17 +10,24 @@ import { UploaderComponent } from "./../uploader/uploader.component";
 })
 export class NewRecordComponent implements OnInit {
   hasFile = false;
-  _subscription
-  @Input() uploader : UploaderComponent;
+  isValid;
+  _subscription;
+  linearMode = true;
+  firstForm: FormGroup;
+
+  @Input() uploader: UploaderComponent;
   constructor(
-   private fileInputService : FileInputService
-  ) { 
-    console.log(this.hasFile);
-    this.hasFile = this.fileInputService.file;
-    this._subscription = fileInputService.fileChange.subscribe((value) => { 
-      this.hasFile = value; 
-      console.log(this.hasFile);
-      
+    private fileInputService: FileInputService,
+    private fb: FormBuilder
+  ) {
+    this.firstForm = this.fb.group({
+      firstCtrl: ['', this.stepValidator],
+    });
+
+
+    this._subscription = fileInputService.fileChange.subscribe((value) => {
+      this.hasFile = value;
+      this.isValid = value;
     });
   }
 
@@ -26,4 +35,14 @@ export class NewRecordComponent implements OnInit {
   }
 
 
+  stepValidator() {
+
+
+  //  if (!this.isValid) {
+  //    return { fileNotFound: true };
+  //  }
+  //  else{
+  //    return null
+  //  }
+  }
 }
