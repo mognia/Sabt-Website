922994816604201cc0db77d8fef1ee9e7f1a85d1
mognia <mognia420@gmail.com>
2019-04-02 00:28:46 +0430

    feature-phone number validation check

--- src/app/home/components/login/login.component.ts
+++ src/app/home/components/login/login.component.ts
@@ -10,12 +10,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 	phoneForm = new FormGroup({
 		phone: new FormControl(undefined, [Validators.required])
-	});
+  });
+  invalidNum;
   constructor(
     public dialogRef: MatDialogRef<LoginComponent>,
   ) { }
 
   ngOnInit() {
   }
-
+  validateNum(){
+   let userPhone = this.phoneForm.controls['phone'];
+   if (userPhone.errors !=null) { 
+     this.invalidNum=true;
+     console.log('invalid num');
+     
+   }else{
+    this.invalidNum=false;
+     console.log('valid num');
+     
+   }
+  }
 }
